f53b8ea7ad5f7c30c65de986ccb7641eeb8b8b28
Guilherme Alcassa <131387446+Alcassa@users.noreply.github.com>
2026-05-13 20:04:28 -0300

    Clear Firebase configuration values
    Removed sensitive Firebase configuration values.

--- src/app/data/firebaseConfig.ts
+++ src/app/data/firebaseConfig.ts
@@ -3,13 +3,13 @@ import { getFirestore } from "firebase/firestore";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAc4LSb58t1PpCjVDLRnyWMwRi4-tZ0tgU",
-  authDomain: "projeto-arandu-teste.firebaseapp.com",
-  projectId: "projeto-arandu-teste",
-  storageBucket: "projeto-arandu-teste.firebasestorage.app",
-  messagingSenderId: "369952857730",
-  appId: "1:369952857730:web:73c4fb1bbc9b362ecade9b",
-  measurementId: "G-4S1H042818"
+  apiKey: 
+  authDomain:
+  projectId: 
+  storageBucket: 
+  messagingSenderId: 
+  appId: 
+  measurementId: 
 };
 
 // Initialize Firebase
